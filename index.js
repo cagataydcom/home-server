8086a86826cee463e55b94f320788ce22e7dcd4d
CagatayD <32710235+cagdu@users.noreply.github.com>
2025-01-07 08:57:15 +0300

    new bugs
    i know...

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-require("dotenv").config();
+const express = require("express"), app = express(), http = require("node:http"), _env = require("dotenv").config();
 
-const express = require("express"), app = express();
-app.use(express.static("./lib/public")).set("view engine", "ejs").set("views", "./lib/views/")
-    .use("/gopc", require("./lib/routers/upload"))
-    .use((req, res, next) => {
-        try {
-            let directory = req._parsedUrl.pathname.split("/").filter(x => x !== "");
-            let folder = require("node:fs").readdirSync(`./lib/public/${directory.join("/")}`);
+app.use(express.static("./lib/public")).set("view engine", "ejs").set("views", "./lib/views/").get("/", (req) => req.res.render("file-explorer")).use((req) => req.res.sendStatus(404));
 
-            let file = require("node:fs").readFileSync("./lib/private/folder.html", { "encoding": "utf8" });
-            file = file.replace("// <!-- Directory -->", `const directory = ${JSON.stringify([directory, folder])};`);
+const options = { ...(process.env.production !== "dev" ? { key: fs.readFileSync("./lib/key.pem"), cert: fs.readFileSync("./lib/cert.pem") } : {}) },
+    server = http.createServer(options, app).listen(process.env.production !== "dev" ? 443 : 80);
 
-            return res.send(file);
-        } catch { return next() }
-    }, (req, res, next) => res.sendStatus(404));
-
-const server = app.listen(process.env.production === "dev" ? 8081 : 80, () => { });
 server.on("upgrade", require("./lib/ws"));
+
+const path = require("node:path"), electron = require("electron/main");
+let mainWindow, tray;
+
+electron.app.on("ready", () => {
+    mainWindow = new electron.BrowserWindow({ autoHideMenuBar: true, frame: true, icon: path.join(__dirname, "/lib/public/assets/img/logo.png"), titleBarStyle: "hiddenInset", title: "Dosya Gezgini", height: 600, webPreferences: { nodeIntegration: false, contextIsolation: true }, width: 1024 });
+    mainWindow.loadURL(`http://localhost:${process.env.production !== "dev" ? 443 : 80}`);
+    mainWindow.on("close", e => { electron.app.isQuiting ? null : e.preventDefault(); mainWindow.hide(); electron.app.hidden = true; });
+
+    tray = new electron.Tray(path.join(__dirname, "/lib/public/favicon.ico"));
+    tray.addListener("click", () => mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show());
+    tray.setContextMenu(electron.Menu.buildFromTemplate([{ label: "Çıkış Yap", click: () => { electron.app.isQuiting = true; electron.app.quit() } }]))
+    tray.setToolTip("Dosya Gezgini");
+});
+
+electron.app.on('window-all-closed', () => { if (process.platform !== 'darwin') { electron.app.quit() } });
